1ef4717238119bb45c3585301fde7bba09fd21db
RazvanMacovei15 <razvanmc15@gmail.com>
2023-03-10 12:56:48 +0200

    public static void change

--- src/calcule/Calcule.java
+++ src/calcule/Calcule.java
@@ -7,6 +7,7 @@ public class Calcule {
     public static float adunare(float num1, float num2) {
         return  num1 + num2;
         //return result;
+        //lalalala
     }
 
     //method pentru scadere
